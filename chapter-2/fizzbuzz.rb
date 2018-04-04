f3209f82f43525075be2b00d18e39f5f9aa47c13
ShinsukeKido <shinsukekido@shinsukenoMac-mini.local>
2018-04-04 14:17:23 +0900

    Fizzbuzz 1 to 100

---
+++ chapter-2/fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz
+  n = 1
+  while n <= 100 do
+    if (n % 3 == 0) && (n % 5 == 0)
+      puts "FizzBuzz"
+    elsif (n % 3) == 0
+      puts "Fizz"
+    elsif (n % 5) == 0
+      puts "Buzz"
+    else
+      puts n
+    end
+    n = n + 1
+  end
+end
+
+fizzbuzz
