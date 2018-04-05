5305f1fc2223df0771f6c5364805e73ee4ef7af0
ShinsukeKido <shinsukekido@shinsukenoMac-mini.local>
2018-04-05 10:42:11 +0900

    Fixed after review

--- chapter-2/fizzbuzz.rb
+++ chapter-2/fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz
   n = 1
   while n <= 100 do
-    if (n % 3 == 0) && (n % 5 == 0)
+    if n % 3 == 0 && n % 5 == 0
       puts "FizzBuzz"
-    elsif (n % 3) == 0
+    elsif n % 3 == 0
       puts "Fizz"
-    elsif (n % 5) == 0
+    elsif n % 5 == 0
       puts "Buzz"
     else
       puts n
