8308393209944a5896ff8a84ac4e203c72540c6f
ShinsukeKido <downmap555@gmail.com>
2018-04-11 12:27:13 +0900

    Unit conversion program

---
+++ chapter-5/unit_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require './unit.rb'
+
+class UnitTest < Minitest::Test
+  def test_unit
+    assert_equal 3.28, convert_length(1)
+    assert_equal 60.02, convert_length(5, from: :ft, to: :in)
+    assert_equal 0.25, convert_length(10, from: :in, to: :m)
+  end
+end
