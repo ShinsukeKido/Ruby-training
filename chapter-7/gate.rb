971c6588e41464ff84aa03adbde34faad0e989c2
ShinsukeKido <downmap555@gmail.com>
2018-04-17 12:56:08 +0900

    乗車駅、降車駅、購買チケットの関係を示すために、Gateクラス、Ticketクラスを作成

---
+++ chapter-7/gate.rb
@@ -0,0 +1,37 @@
+class Gate
+  STATIONS = [:umeda, :juso, :mikuni]
+  FARES = [150, 190]
+
+  def initialize(name)
+    @name = name
+  end
+
+  def enter(ticket)
+    ticket.stamp(@name)
+  end
+
+  def exit(ticket)
+    real_fare = calc_fare(ticket)
+    real_fare <= ticket.paid_fare
+  end
+
+  private
+  def calc_fare(ticket)
+    from = STATIONS.index(ticket.stamped_at)
+    to = STATIONS.index(@name)
+    distance = to - from
+    FARES[distance - 1]
+  end
+end
+
+class Ticket
+  attr_reader :paid_fare, :stamped_at
+
+  def initialize(fare)
+    @paid_fare = fare
+  end
+
+  def stamp(name)
+    @stamped_at = name
+  end
+end
