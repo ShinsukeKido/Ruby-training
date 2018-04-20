37bd375ba434a651d2c8ac110ca90d2bf230b628
ShinsukeKido <downmap555@gmail.com>
2018-04-20 10:44:57 +0900

    RSpecテストコードの、describe名、ネスト構造を変更

--- spec/bank_class_spec.rb
+++ spec/bank_class_spec.rb
@@ -1,16 +1,19 @@
 require './chapter-8/team.rb'
 require './chapter-8/bank.rb'
 
-RSpec.describe 'Bankクラスのハッシュ定数CURRENCIESの全体と各要素をfreezeさせる' do
+RSpec.describe 'Bank' do
   it 'freezeされたCURRENCIESの値が正しいか' do
     expect(Bank::CURRENCIES).to eq ({'Japan' => 'yen', 'US' => 'dollar', 'India' => 'rupee'})
   end
 
-  it 'CURRENCIES全体がfreezeされているか' do
-    expect(Bank::CURRENCIES.frozen?).to eq true
-  end
+  describe '.deep_freeze' do
+    it 'CURRENCIES全体がfreezeされているか' do
+      expect(Bank::CURRENCIES.frozen?).to eq true
+    end
 
-  it 'CURRENCIESの各要素がfreezeされているか' do
-    expect(Bank::CURRENCIES.all?{ |key, value| key.frozen? && value.frozen? }).to eq true
+    it 'CURRENCIESの各要素がfreezeされているか' do
+      expect(Bank::CURRENCIES.all?{ |key, value| key.frozen? && value.frozen? }).to eq true
+    end
   end
 end
+
--- spec/team_class_spec.rb
+++ spec/team_class_spec.rb
@@ -1,17 +1,19 @@
 require './chapter-8/team.rb'
 require './chapter-8/bank.rb'
 
-RSpec.describe 'Teamクラスの配列定数COUNTRIESの全体と各要素をfreezeさせる' do
+RSpec.describe 'Team' do
   it 'freezeされたCOUNTRIESの値が正しいか' do
     expect(Team::COUNTRIES).to eq ['Japan', 'US', 'India']
   end
 
-  it 'COUNTRIES全体がfreezeされているか' do
-    expect(Team::COUNTRIES.frozen?).to eq true
-  end
+  describe '.deep_freeze' do
+    it 'COUNTRIES全体がfreezeされているか' do
+      expect(Team::COUNTRIES.frozen?).to eq true
+    end
 
-  it 'COUNTRIESの各要素がfreezeされているか' do
-    expect(Team::COUNTRIES.all?{ |country| country.frozen? }).to eq true
+    it 'COUNTRIESの各要素がfreezeされているか' do
+      expect(Team::COUNTRIES.all?{ |country| country.frozen? }).to eq true
+    end
   end
 end
 
