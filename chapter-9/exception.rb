bb29175d72c737a35e707044e86c73ba589591f8
ShinsukeKido <downmap555@gmail.com>
2018-04-24 14:39:11 +0900

    正規表現の作成を/ /から、Regexp.newに変更(内部に\を入力した時にエラーになるため。)

--- chapter-9/exception.rb
+++ chapter-9/exception.rb
@@ -9,7 +9,7 @@ def input_regexp
   print 'Pattern?: '
   pattern = gets.chomp
   return input_regexp if pattern == ''
-  /#{pattern}/
+  Regexp.new(pattern)
 rescue RegexpError => e
   puts "Invalid pattern: #{e.message}"
   retry
