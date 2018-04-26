52ccbeb45cc0cda8e98d3a4d71d7847a478c7a68
ShinsukeKido <downmap555@gmail.com>
2018-04-26 17:53:07 +0900

    無効な正規表現の入力を6回行うとRegexpErrorと表示させるように変更

--- chapter-9/exception.rb
+++ chapter-9/exception.rb
@@ -8,6 +8,7 @@ class RegexpTest
   end
 
   def match_text_to_regexp(text, regexp)
+    return 'RegexpError' if regexp == 'Error'
     matches = text.scan(regexp)
     if matches.size.positive?
       "Matched: #{matches.join(', ')}"
@@ -29,15 +30,23 @@ class RegexpTest
   end
 
   def input_regexp
-    print 'Pattern?: '
-    pattern = gets.chomp
-    raise EmptyInputError if pattern.empty?
-    Regexp.new(pattern)
-  rescue EmptyInputError
-    puts 'Please input something'
-    retry
-  rescue RegexpError => e
-    puts "Invalid pattern: #{e.message}"
-    retry
+    retry_count = 0
+    begin
+      print 'Pattern?: '
+      pattern = gets.chomp
+      raise EmptyInputError if pattern.empty?
+      Regexp.new(pattern)
+    rescue EmptyInputError
+      puts 'Please input something'
+      retry
+    rescue RegexpError => e
+      retry_count += 1
+      if retry_count <= 5
+        puts "Invalid pattern: #{e.message}"
+        retry
+      else
+        'Error'
+      end
+    end
   end
 end
