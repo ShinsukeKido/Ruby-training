ee551cb202bbe6a21448887239781c31e7e7c81d
ShinsukeKido <downmap555@gmail.com>
2018-05-02 10:08:57 +0900

    RSpecにおいて、subjectを用いた構文に変更

--- spec/effects_spec.rb
+++ spec/effects_spec.rb
@@ -1,10 +1,11 @@
 require './chapter-10/effects.rb'
 
 RSpec.describe Effects do
+  subject { effect.call('Ruby is fun!') }
   describe '.reverse' do
     let(:effect) { Effects.reverse }
     it 'reverseメソッドにより作成されたラムダが、与えられた文字列の各単語の順序を逆にする' do
-      expect(effect.call('Ruby is fun!')).to eq 'ybuR si !nuf'
+      is_expected.to eq 'ybuR si !nuf'
     end
   end
 
@@ -12,14 +13,14 @@ RSpec.describe Effects do
     context 'echoメソッドの引数が2の時' do
       let(:effect) { Effects.echo(2) }
       it 'echo(2)メソッドにより作成されたラムダが、与えられた文字列の各文字を2回繰り返す' do
-        expect(effect.call('Ruby is fun!')).to eq 'RRuubbyy iiss ffuunn!!'
+        is_expected.to eq 'RRuubbyy iiss ffuunn!!'
       end
     end
 
     context 'echoメソッドの引数が3の時' do
       let(:effect) { Effects.echo(3) }
       it 'echo(3)メソッドにより作成されたラムダが、与えられた文字列の各文字を3回繰り返す' do
-        expect(effect.call('Ruby is fun!')).to eq 'RRRuuubbbyyy iiisss fffuuunnn!!!'
+        is_expected.to eq 'RRRuuubbbyyy iiisss fffuuunnn!!!'
       end
     end
   end
@@ -28,14 +29,14 @@ RSpec.describe Effects do
     context 'loudメソッドの引数が2の時' do
       let(:effect) { Effects.loud(2) }
       it 'loud(2)メソッドにより作成されたラムダが、与えられた文字列を大文字にし、末尾に!を2つ追加する' do
-        expect(effect.call('Ruby is fun!')).to eq 'RUBY!! IS!! FUN!!!'
+        is_expected.to eq 'RUBY!! IS!! FUN!!!'
       end
     end
 
     context 'loudメソッドの引数が3の時' do
       let(:effect) { Effects.loud(3) }
       it 'loud(3)メソッドにより作成されたラムダが、与えられた文字列を大文字にし、末尾に!を3つ追加する' do
-        expect(effect.call('Ruby is fun!')).to eq 'RUBY!!! IS!!! FUN!!!!'
+        is_expected.to eq 'RUBY!!! IS!!! FUN!!!!'
       end
     end
   end
